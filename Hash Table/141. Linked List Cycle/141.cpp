1b47659c6344e3aa87f5ae3f6af01c1dba9ef548
Tsung-Han Hsieh <64431591+Lucashien@users.noreply.github.com>
2024-02-07 19:12:54 +0800

    141. Linked List Cycle

---
+++ Hash Table/141. Linked List Cycle/141.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // using fast and slow to judge
+        // In the end, fast will trace the slow
+
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            
+            // fast goes 2 step, slow goes 1 step
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+};
