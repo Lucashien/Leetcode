8bc0d6e59cf0e4ab403b9450b2c07a7d72f35b98
Tsung-Han Hsieh <64431591+Lucashien@users.noreply.github.com>
2024-02-08 18:23:34 +0800

    Update 17.cpp add the recursive method

--- Hash Table/17. Letter Combinations of a Phone Number/17.cpp
+++ Hash Table/17. Letter Combinations of a Phone Number/17.cpp
@@ -1,3 +1,4 @@
+/*for loop way (BFS)*/
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
@@ -24,5 +25,51 @@ public:
             ans = temp;
         }
         return ans;
+    }
+};
+
+
+/*Recursive way (DFS)*/
+class Solution {
+public:
+    unordered_map <int, string> mapping;
+    string ans = "";
+    // component: 
+    void solve(int idx,string& digits,vector<string>& final_ans ){
+
+        if(idx>=digits.size()){
+            final_ans.push_back(ans);
+            return;
+        }
+
+        int num = digits[idx] - '0'; // 數字鍵
+        string str = mapping[num];
+        int size = str.size(); // 有幾個字母
+        
+        for(int i = 0;i<size;i++){
+            ans +=str[i];
+            solve(idx+1,digits,final_ans);
+            ans.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        mapping[2]=  {"abc"};
+        mapping[3]=  {"def"};
+        mapping[4]=  {"ghi"};
+        mapping[5]=  {"jkl"};
+        mapping[6]=  {"mno"};
+        mapping[7]=  {"pqrs"};
+        mapping[8]=  {"tuv"};
+        mapping[9]=  {"wxyz"};
+
+        vector<string> final_ans;
+        if(digits.size()==0){
+            return final_ans;
+        }
+
+        solve(0,digits,final_ans);
+        return final_ans;
+
     }
 };
